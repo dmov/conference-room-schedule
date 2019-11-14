ec700f1cf696e1a2f8ae52136e4daac07ef0d34f
dmov <56564005+dmov@users.noreply.github.com>
2019-11-14 15:15:07 +0300

    Update Main class
    Making variables final

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ConferenceRoom conferenceRoom = new ConferenceRoom();
+        final ConferenceRoom conferenceRoom = new ConferenceRoom();
         conferenceRoom.addEvent(new Event("2019-10-15 10:00", "2019-10-15 11:00", "First event"));
         conferenceRoom.addEvent(new Event("2019-10-15 11:30", "2019-10-15 13:00", "Second event"));
         conferenceRoom.addEvent(new Event("2019-10-15 15:00", "2019-10-15 17:00", "Third event"));
 
-        Scanner in = new Scanner(System.in);
+        final Scanner in = new Scanner(System.in);
 
         System.out.print("Input event's name: ");
-        String eventName = in.nextLine();
+        final String eventName = in.nextLine();
 
         System.out.print("Input event's begin date and time: ");
-        String eventBeginTime = in.nextLine();
+        final String eventBeginTime = in.nextLine();
 
         System.out.print("Input event's end date and time: ");
-        String eventEndTime = in.nextLine();
+        final String eventEndTime = in.nextLine();
 
         conferenceRoom.addEvent(new Event(eventBeginTime, eventEndTime, eventName));
     }
